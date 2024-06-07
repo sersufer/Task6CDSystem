e6687f2e21428b6fcf3b8c08f8830e9884b6eb46
Sergio Suñen <50202438+sersufer@users.noreply.github.com>
2024-06-07 20:33:22 +0200

    Update romans.js

--- romans.js
+++ romans.js
@@ -1,20 +1,3 @@
-// Load Google Analytics gtag.js and configure it
-(function() {
-  var gtagScript = document.createElement('script');
-  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-R7L913Q2PN';
-  gtagScript.async = true;
-  document.head.appendChild(gtagScript);
-
-  gtagScript.onload = function() {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    window.gtag = gtag;
-    gtag('js', new Date());
-    gtag('config', 'G-R7L913Q2PN');
-  };
-})();
 
 // Constants for the literals
 const INVALID_ROMAN = 'Please enter a valid roman';
