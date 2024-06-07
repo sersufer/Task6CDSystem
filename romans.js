c1750cf4cec203896a070abd5b39a8c4bcdc8e18
Sergio Suñen <50202438+sersufer@users.noreply.github.com>
2024-06-07 20:39:01 +0200

    Update romans.js

--- romans.js
+++ romans.js
@@ -1,5 +1,22 @@
+ $(document).ready( function(){
+  // Create the script element for Google Tag Manager
+  var gtmScript = document.createElement('script');
+  gtmScript.src = "https://www.googletagmanager.com/gtag/js?id=G-R7L913Q2PN";
+  gtmScript.async = true;
 
-import { gtag } from 'https://www.googletagmanager.com/gtag/js?id=G-R7L913Q2PN';
+  // Create the script element for configuring Google Analytics
+  var gaScript = document.createElement('script');
+  gaScript.textContent = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'yourID');
+  `;
+
+  // Append the scripts to the head of the document
+  document.head.appendChild(gtmScript);
+  document.head.appendChild(gaScript);
+});
 
 // Constants for the literals
 const INVALID_ROMAN = 'Please enter a valid roman';
